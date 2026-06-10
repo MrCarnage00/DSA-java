40ab23425d1740e2657a4630a5f946145a939236
Priyanshu Sharma <priyanshushr133@gmail.com>
2026-06-10 23:53:31 +0530

    Add Kadane's Algorithm solution for Maximum Subarray

---
+++ arrays/53_Maximum_Subarray.java
@@ -0,0 +1,19 @@
+/*
+Problem: 53. Maximum Subarray
+Pattern: Kadane's Algorithm
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class _53_Maximum_Subarray{
+    public int maxSubArray(int[] nums) {
+        int cs = nums[0];
+        int ms = nums[0];
+
+        for(int i = 1 ; i < nums.length ; i++){
+            cs = Math.max(nums[i] , cs + nums[i]);
+            ms = Math.max(ms,cs);
+        }
+        return ms;
+    }
+}
